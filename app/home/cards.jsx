a14fee19bdcaba62b5b75c578220357805f8b6b1
tushar7789 <tusharjangid7@gmail.com>
2024-02-08 23:58:44 +0530

    created cards container

---
+++ app/home/cards.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './cards.css'
+
+function cards () {
+    return (
+        <div id="card-container">
+            <div id="card-upper-container">
+
+            </div>
+            <div id="card-lower-container">
+
+            </div>
+        </div>
+    )
+}
+
+export default cards 
--- app/home/page.jsx
+++ app/home/page.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './style.css'
 import Navbar from './navbar'
 import Banner from './banner'
+import Cards from './cards'
 
 const HomePage = () => {
   return (
     <>
         <Navbar/>
         <Banner/>
+        {/* <Cards/> */}
     </>
   )
 }
